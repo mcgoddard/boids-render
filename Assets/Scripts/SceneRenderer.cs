b2cdf23292f8eeaa4cf5d410b4ef4c34b0d58d99
Michael Goddard <mikeygiom@gmail.com>
2017-12-20 00:23:20 +0000

    Added the boids program and implemented rendering on top. Hits >100fps with 100 boids on AMD8350. Rendering functional, boid directions not working as intended and flocking algorithm could use improvement.

--- Assets/Scripts/SceneRenderer.cs
+++ Assets/Scripts/SceneRenderer.cs
@@ -1,6 +1,9 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
@@ -14,72 +17,103 @@ public class SceneRenderer : MonoBehaviour {
     public GameObject PurpleBoid;
     public GameObject YellowBoid;
 
-    private IPEndPoint endpoint = new IPEndPoint(IPAddress.Loopback, 4794);
     private UdpClient udp;
-    private Dictionary<int, GameObject> objects;
+    private Queue<Dictionary<string, object>> newObjects;
+    private Dictionary<long, GameObject> objects;
     private Thread readThread;
+    private Process boidsProc;
     private bool running = true;
 
 	// Use this for initialization
 	void Start ()
     {
-        objects = new Dictionary<int, GameObject>();
-        udp = new UdpClient();
+        newObjects = new Queue<Dictionary<string, object>>();
+        objects = new Dictionary<long, GameObject>();
+        udp = new UdpClient(4794);
         readThread = new Thread(Listener);
         readThread.Start();
+        boidsProc = new Process();
+        boidsProc.StartInfo.UseShellExecute = false;
+        boidsProc.StartInfo.FileName = "rust-boids.exe";
+        boidsProc.StartInfo.CreateNoWindow = true;
+        boidsProc.Start();
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
+        while (newObjects.Count > 0)
+        {
+            var newObject = newObjects.Dequeue();
+            if (newObject != null && newObject.ContainsKey("id") && newObject["id"] != null)
+            {
+                var id = (long)newObject["id"];
+                if (!objects.ContainsKey(id))
+                {
+                    objects.Add((long)newObject["id"], InitialiseBoid((string)newObject["colour"]));
+                }
+                var newPos = (IDictionary<string, JToken>)newObject["position"];
+                var newDirection = (IDictionary<string, JToken>)newObject["direction"];
+                try
+                {
+                    float x = newPos["x"].Value<float>();
+                    float y = newPos["y"].Value<float>();
+                    float z = newPos["z"].Value<float>();
+                    Vector3 newPosVec = new Vector3(x, y, z);
+                    objects[id].transform.position = newPosVec;
+                    float xD = newDirection["x"].Value<float>();
+                    float yD = newDirection["y"].Value<float>();
+                    float zD = newDirection["z"].Value<float>();
+                    objects[id].transform.rotation = Quaternion.Euler(xD, yD, zD);
+                }
+                catch (Exception) { }
+            }
+        }
     }
 
     // Called during shutdown
     void OnDestroy()
     {
         running = false;
+        boidsProc.Kill();
     }
 
     private void Listener()
     {
         while (running)
         {
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Loopback, 0);
             byte[] receiveBytes = udp.Receive(ref endpoint);
             string serialisedObj = System.Text.Encoding.Default.GetString(receiveBytes);
             Dictionary<string, object> deserialisedObj = JsonConvert.DeserializeObject<Dictionary<string, object>>(serialisedObj);
-            int id = (int)deserialisedObj["id"];
-            if (!objects.ContainsKey(id))
+            if (deserialisedObj.ContainsKey("id"))
             {
-                objects[id] = InitialiseBoid((string)deserialisedObj["colour"]);
+                long id = (long)deserialisedObj["id"];
+                newObjects.Enqueue(deserialisedObj);
             }
-            Dictionary<string, float> newPos = (Dictionary<string, float>)deserialisedObj["position"];
-            Vector3 newPosVec = new Vector3(newPos["x"], newPos["y"], newPos["z"]);
-            objects[id].transform.position = newPosVec;
-            Dictionary<string, float> newDir = (Dictionary<string, float>)deserialisedObj["direction"];
-            objects[id].transform.rotation = Quaternion.Euler(newDir["x"], newDir["y"], newDir["z"]);
         }
     }
 
     private GameObject InitialiseBoid(string colour)
     {
         GameObject newBoid;
-        if (colour.Equals("green"))
+        if (colour.Equals("Green"))
         {
             newBoid = GameObject.Instantiate(GreenBoid);
         }
-        if (colour.Equals("blue"))
+        if (colour.Equals("Blue"))
         {
             newBoid = GameObject.Instantiate(BlueBoid);
         }
-        if (colour.Equals("red"))
+        if (colour.Equals("Red"))
         {
             newBoid = GameObject.Instantiate(RedBoid);
         }
-        if (colour.Equals("orange"))
+        if (colour.Equals("Orange"))
         {
             newBoid = GameObject.Instantiate(OrangeBoid);
         }
-        if (colour.Equals("purple"))
+        if (colour.Equals("Purple"))
         {
             newBoid = GameObject.Instantiate(PurpleBoid);
         }
---
+++ rust-boids.d
@@ -0,0 +1,1 @@
+E:\Programming\Rust\rust-boids\target\release\rust-boids.exe: E:\Programming\Rust\rust-boids\src\main.rs
