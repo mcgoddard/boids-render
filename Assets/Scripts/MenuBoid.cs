f095735a5f4614c7ac6e936f894d9f9a5e75d987
Michael Goddard <mikeygiom@gmail.com>
2018-01-06 17:12:06 +0000

    Fixed normals on boid model and rebuilt prefabs. Added menu scene to load main boid scene passing number of boids to use.

---
+++ Assets/Scripts/MenuBoid.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuBoid : MonoBehaviour
+{
+    const float ROTATE = 90;
+
+	// Use this for initialization
+	void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Rotate(0, ROTATE * Time.deltaTime, 0);
+	}
+}
---
+++ Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuHandler : MonoBehaviour
+{
+    public Text countField;
+    public Image countFieldBackground;
+
+    private int boidNum = 500;
+
+	// Use this for initialization
+	void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	}
+    
+    public void StartClick()
+    {
+        if (boidNum < 0 || boidNum > 10000)
+        {
+            countFieldBackground.color = Color.red;
+        }
+        else
+        {
+            PlayerPrefs.SetInt("boidsCount", boidNum);
+            Debug.Log(String.Format("Starting with {0} boids", boidNum));
+            SceneManager.LoadScene("boids-render", LoadSceneMode.Single);
+        }
+    }
+
+    public void BoidsCountChanged()
+    {
+        string text = countField.text;
+        int newCount;
+        if (int.TryParse(text, out newCount))
+        {
+            countFieldBackground.color = Color.white;
+            boidNum = newCount;
+        }
+        else
+        {
+            countFieldBackground.color = Color.red;
+            countField.text = countField.ToString();
+        }
+    }
+}
